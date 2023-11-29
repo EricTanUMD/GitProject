2891e9145983c12a0a6355cd3444544aaa35a7d5
Eric Tan <etan123@terpmail.umd.edu>
2023-11-29 11:55:12 -0500

    Updated code, Uses Dictionary now

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,10 @@
-# Required Imports, everything is installed via pip
+# This file serves to scrape informations of the wikipedia page that documents the largest companies in the United states by revenue.
+# This information will then be sequentially fed into a series of lists, a dictionary, and then be converted into a dataframe
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import os
+import re
 
 #Use Beautiful soup to specificly scrape the first chart for its information
 url = "https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue"
@@ -13,7 +15,6 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 
 #Finding the correct table
-print()
 table = soup.find_all('table')[1] #We found the table we want here
 base_titles = table.find_all('th') # we extract the tiles marked by </th>
 titles = [title.text.strip() for title in base_titles] # remove </th> and new lines, only a list of base strings remain
@@ -22,13 +23,55 @@ titles = [title.text.strip() for title in base_titles] # remove </th> and new li
 df = pd.DataFrame(columns = titles) #create our dataframe with columns
 column_data = table.find_all('tr')
 
-#Extract the row data from each column, but these are still individual lists
+# Extract the information of each row into a list of its own
+rank = []
+name = []
+industry = []
+revenue = []
+revenue_growth = []
+employees = []
+headquarters = []
+
+counter = 1
 for row in column_data[1:]: #skip first blank row
     row_data = row.find_all('td')
-    individual_row_data = [data.text.strip() for data in row_data]
-    # print(individual_row_data)
-    df_length = len(df) 
-    df.loc[df_length] = individual_row_data # check for the length of the dataframe and we just append each row onto it, pretty cool
+    #print(row_data)
+    for data in row_data:
+        if(counter == 1):
+            rank.append(int(data.text.strip()))
+            counter += 1
+        elif(counter == 2):
+            name.append(data.text.strip())
+            counter += 1
+        elif (counter == 3):
+            industry.append(data.text.strip())
+            counter += 1
+        elif (counter == 4):
+            revenue.append(float(re.sub("[,]", "", data.text.strip())))
+            counter += 1
+        elif (counter == 5):
+            revenue_growth.append(float(re.sub("[\%]", "", data.text.strip()))) #this is a percent, convert for specific calculations
+            counter += 1
+        elif (counter == 6):
+            employees.append(float(re.sub("[,]", "", data.text.strip()))) #convert into a float
+            counter += 1
+        elif (counter == 7):
+            headquarters.append(data.text.strip())
+            counter = 1
 
-print(df)
-df.to_csv(os.getcwd() + '/df.csv', index = False) #import the constructed dataframe as a csv file!
+#print(headquarters)
+#print(len(rank) + len(name) + len(industry) + len(revenue) + len(revenue_growth) + len(employees) + len(headquarters))
+
+Companies = {
+    "rank": rank,
+    "name": name,
+    "industry": industry,
+    "revenue": revenue,
+    "revenue_growth": revenue_growth,
+    "employees": employees,
+    "headquarters": headquarters
+}
+
+data_frame = pd.DataFrame.from_dict(Companies)
+print(data_frame)
+data_frame.to_csv(os.getcwd() + '/df.csv', index = False) #import the constructed dataframe as a csv file!
