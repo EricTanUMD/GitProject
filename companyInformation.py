2fc69e2ac00ef31aea8fbf5079ceddb437c2336d
Eric Tan <etan123@terpmail.umd.edu>
2023-12-06 10:43:19 -0500

    implemented Find functionality

--- companyInformation.py
+++ companyInformation.py
@@ -30,6 +30,18 @@ def find_revenue(lower, upper):
         counter += 1
     return revenue, counter
 
+def find_employees(lower, upper):
+    """ Find the revenues within the range of the ranks specified"""
+    if(lower <= 0 or upper > 100):
+        raise Exception("Sorry no lower limit below 1 or upper limit above 100")
+    
+    employees = []
+    counter = 0
+    for emp in ws.Companies["employees"][lower - 1: upper]:
+        employees.append(emp)
+        counter += 1
+    return employees, counter
+
 def remove_from_list(lower, upper, list): #try to iteratively remove from the list
     """ Removes everything from a list within the specified range of the list"""
     
@@ -37,27 +49,53 @@ def remove_from_list(lower, upper, list): #try to iteratively remove from the li
 ################################################################################################################
 # Main Code
 loop = True
-options = {"Find ranks ", 
+options = ("Find ranks", 
            "Find revenue", 
-           "Find employee number ",
+           "Find employee number",
            "Find revenue growth", 
-           "Average revenue ", 
-           "Revenue per employee "} #The user can only pick from these choices
+           "Average revenue", 
+           "Revenue per employee") #The user can only pick from these choices
 
 print("Welcome to the Company Ranking DataBase")
+result_log = []
+result_num = 0
+counter = 0
 while (loop == True):
     print("Here are your options, ", options)
-    choice = input("What do you wish to do?:\n")
+    choice = input("What do you wish to do?: ")
+    print(choice)
+    print(options[0].casefold())
     # Handle each function depending on
-    if(choice.casefold() == options[0].casefold()): # consider adding a string to store the tuple results using variables.
-        lower_limit = input("Enter the lowest rank you want to find: ")
-        upper_limit = input("Enter the highest rank you want to find: ")
-        print()
+    if(choice.casefold() == options[0].casefold()): # Find Ranks
+        lower_limit = int(input("Enter the lowest rank you want to find: "))
+        upper_limit = int(input("Enter the highest rank you want to find: "))
+        result_log, result_num = find_ranks(lower_limit, upper_limit)
+        print("Here are the ranks in order: {}. You searched through {} companies.", result_log, result_num)
+    elif(choice.casefold() == options[1].casefold()): # Find Revenue
+        lower_limit = int(input("Enter the lowest rank you want to find revenue: "))
+        upper_limit = int(input("Enter the highest rank you want to find revenue: "))
+        result_log, result_num = find_revenue(lower_limit, upper_limit)
+        print("Here are the revenues in millions USD in order from largest to smallest {}. You searched through {} companies.".format(result_log, result_num))
+    elif(choice.casefold() == options[2].casefold()): # Employee Numbers
+        lower_limit = int(input("Enter the lowest rank you want to find employee numbers: "))
+        upper_limit = int(input("Enter the highest rank you want to find employee number: "))
+    elif(): # Revenue Growth
+        print("placeholder")
+    elif(): # Average revenue find
+        print("placeholder")
+    else: # If the user fails to input anything, they will have up to 3 chances input a valid input
+        counter += 1
+        print("Invalid Input you have failed {} times. Program exits after 3 fails.".format(counter))
+
+
+    if (counter == 3):
+        print("Too many invalid inputs rerun the program")
+        break
 
     #Note to self, you can convert a df column into an array
-    if (input("Do you wish to continue?") == "yes".casefold()):
+    if (input("Do you wish to continue?") == "no".casefold()):
         loop = False
-        break
+        print("Thanks for using the program, exiting now")
 
 
 
